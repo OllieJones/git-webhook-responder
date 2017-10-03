ebb4999a7360f8086a4ad141415c174ecf04fd94
Ollie Jones <ollie@video-d.myglance.org>
2017-10-03 19:03:58 +0000

    spawn

--- routes/webhook.js
+++ routes/webhook.js
@@ -9,7 +9,26 @@ router.post( '/', function( req, res, next ) {
 
   var status;
   var output;
-  if( req.description && req.description.ok ) {
+    if( req.description && req.description.ok ) {
+	/* here's where we do something useful */
+
+	const myShell = spawn ('on-webhook', [req.description.git_ssh_url,
+					      req.description.reponame,
+					      req.description.event_name,
+					      req.description.ref ]);
+
+	myShell.stdout.on('data', function(data) {
+	    console.log ('stdout from shell', data.toString('utf8'));
+	});
+	myShell.stderr.on('data', function(data) {
+	    console.log ('stderr from shell', data.toString('utf8'));
+	});
+        myShell.on('close', function(code){
+             console.log ('shell ended with code', code);
+        });
+	myShell.stdin.write(JSON.stringify(req.description.payload));
+
+	
     status = 200;
     if( req.query.describe ) {
       req.description.status  = status;
